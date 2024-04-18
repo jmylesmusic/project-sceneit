030f0967641dbe797f0f131f9b6a02587a101341
James Myles <myles.james@gmail.com>
2024-04-18 13:03:08 +0200

    cardFormatting-moviesPageContainer

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,10 @@
 import MovieCardsCarousel from "../components/MovieCardsCarousel";
+import "../styles/movies.css";
 const Movies = () => {
   const url =
     "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=";
   return (
-    <div>
+    <div className="movies-screen-container">
       {/* <MovieCardsCarousel listtype={`${url}/favorite`} /> */}
       <h1>Drama</h1>
       <MovieCardsCarousel listtype={`${url}18`} />
