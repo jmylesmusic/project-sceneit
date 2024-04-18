652c22e08ab367e4b3d3427f52c11877e55e053b
James Myles <myles.james@gmail.com>
2024-04-18 16:50:27 +0200

    finalCommitThursday

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -27,7 +27,7 @@ const MovieCard = (props) => {
         minHeight: "300px", // Ensures that all cards have at least a certain height
         minWidth: "350px",
         margin: "auto",
-        backgroundColor: "var(--lightest)",
+        backgroundColor: "var(--fadedpurple)",
         paddingTop: "3rem",
       }}
     >
